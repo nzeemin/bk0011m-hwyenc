ed12d5f71a68ce47cb974acf5d2136e35daef75d
Nikita Zimin <nzeemin@gmail.com>
2024-04-18 21:33:13 +0300

    Pre-shift for -2px/4px shift sprites

--- !run-bkemugid.cmd
+++ !run-bkemugid.cmd
@@ -11,4 +11,4 @@ bkdecmd a x-bkemugid/Img/ANDOS.IMG HWYENC.BIN
 bkdecmd a x-bkemugid/Img/ANDOS.IMG HWYSCR.LZS
 bkdecmd a x-bkemugid/Img/ANDOS.IMG HWYENC.LZS
 
-start x-bkemugid\BK_x64.exe /B hwyenc.bin /C BK-0011M_FDD
+start x-bkemugid\BK_x64.exe /C BK-0011M_FDD
